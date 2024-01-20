041afafb689664b22bee2149914f07b89cc8b3db
BlueWhaleKo <koko8624@gmail.com>
2024-01-20 15:50:44 +0900

    feat: add generic to Attacker and Evaluator

--- prompt_hacker/interface.py
+++ prompt_hacker/interface.py
@@ -1,7 +1,7 @@
 from abc import abstractmethod
-from typing import Generic, Protocol, TypeVar
+from typing import Protocol
 
-from prompt_hacker.schemas import AttackResult
+from prompt_hacker.schemas import T_EVALUATION_RESULT, EvaluationSummary, T_ATTACK_RESULT_co, T_ATTACK_RESULT_con
 
 
 class ChatBaseModel(Protocol):
@@ -13,17 +13,18 @@ class EmbedBaseModel(Protocol):
     def run(self, txts: list[str], **kwargs) -> list[list[float]]:
         ...
 
-R = TypeVar("R", bound=AttackResult, covariant=True)
 
-class Attacker(Protocol[R]):
+class Attacker(Protocol[T_ATTACK_RESULT_co]):
     @abstractmethod
-    def run(self) -> R:
+    def run(self) -> T_ATTACK_RESULT_co:
         pass
 
 
-class Evaluator(Protocol):
-    def evaluate(self, *args, **kwargs):
+class Evaluator(Protocol[T_ATTACK_RESULT_con, T_EVALUATION_RESULT]):
+    @abstractmethod
+    def evaluate(self, result: T_ATTACK_RESULT_con) -> T_EVALUATION_RESULT:
         pass
 
-    def summary(self, *args, **kwargs):
+    @abstractmethod
+    def summary(self, result: T_EVALUATION_RESULT) -> EvaluationSummary:
         pass
