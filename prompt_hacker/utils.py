d61b30d9cf94cab16783b153f42047e705ea31f6
hskimim <hskimim8855@gmail.com>
2024-01-27 22:38:48 +0900

    Add utility functions for reading JSON files and calculating cosine similarity

--- prompt_hacker/utils.py
+++ prompt_hacker/utils.py
@@ -1,3 +1,7 @@
+import json
+import os
+from typing import Any
+
 import numpy as np
 
 
@@ -12,3 +16,15 @@ def calc_cosine_sim(v1: list[list[float]], v2: list[list[float]]) -> list[float]
     unit_v2 = np.array(v2) / np.linalg.norm(v2, axis=1)
 
     return np.dot(unit_v1, unit_v2.T).reshape(-1)
+
+
+def read_json(fname: str) -> list[dict[str, str]]:
+    with open(fname, "r") as f:
+        doc = f.read()
+    return json.loads(doc)
+
+
+def read_file_with_rel_path(relative_file_path: str) -> Any:
+    module_directory = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.normpath(os.path.join(module_directory, relative_file_path))
+    return read_json(file_path)
