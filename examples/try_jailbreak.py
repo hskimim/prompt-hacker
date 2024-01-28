4051171f836dd82d2c56130abe1eb52102041d19
hskimim <hskimim8855@gmail.com>
2024-01-29 02:30:12 +0900

    update examples

--- examples/try_jailbreak.py
+++ examples/try_jailbreak.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
 from prompt_hacker.attack.jailbreak import JailBreaker, JailBreakEvaluator
-from prompt_hacker.pipe import PipeLine
 from prompt_hacker.schemas import JailBreakInputs
 from prompt_hacker.test.api_client import TestModelClient  # noqa: E402
 
@@ -14,9 +13,14 @@ load_dotenv(verbose=True)
 
 
 if __name__ == "__main__":
-    pipe = PipeLine(
-        attacker=JailBreaker(model=TestModelClient()),
-        evaluator=JailBreakEvaluator(),
-    )
-    report = pipe(JailBreakInputs(sample_size=10))
-    print(report)  # score=0.45454545454545453
+    attacker = JailBreaker(TestModelClient())
+    result = attacker.run(JailBreakInputs(sample_size=10))
+
+    evaluator = JailBreakEvaluator()
+    evaluated = evaluator.evaluate(result)
+    summary = evaluator.summary(evaluated)
+
+    metrics = evaluator.evaluation_metric
+    jailbreak_result_df = evaluator.evaluate2df(evaluated)
+
+    print(summary)  # Evaluation(score=0.5)
