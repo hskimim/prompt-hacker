94037b0e54d1c140d773fd368b12d7fdede85c9b
hskimim <hskimim8855@gmail.com>
2024-02-03 03:18:49 +0900

    Refactor prompt injection and add PromptLeakLoader

--- prompt_hacker/loader/leak.py
+++ prompt_hacker/loader/leak.py
@@ -1,12 +1,12 @@
 from prompt_hacker import constant, utils
 
 
-class PromptInjectLoader:
+class PromptLeakLoader:
     def __init__(
         self,
     ) -> None:
         self._inject_prompts: list[dict[str, str]] = utils.read_file_with_rel_path(
-            constant.INJECT_PROMPTS_JSON_REL_PATH,
+            constant.LEAK_PROMPTS_JSON_REL_PATH,
         )
 
     def load(self) -> list[dict[str, str]]:
